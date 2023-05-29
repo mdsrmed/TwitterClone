8038f653f9c4782a535cd492312876117e625b47
mdsrmed <mdsrmed@gmail.com>
2023-05-28 20:06:36 -0400

    login user and error handling

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -51,6 +51,12 @@ class HomeViewController: UIViewController {
         timelineTableView.delegate = self
         timelineTableView.dataSource = self
         configureNavigationBar()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "rectangle.portrait.and.arrow.right"), style: .plain, target: self, action:#selector(didTapSignOut) )
+    }
+    
+    @objc private func didTapSignOut(){
+        try? Auth.auth().signOut()
+        handleAuthentication()
     }
     
     override func viewDidLayoutSubviews() {
@@ -58,14 +64,21 @@ class HomeViewController: UIViewController {
         timelineTableView.frame = view.frame
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = false
+    
+    private func handleAuthentication(){
         if Auth.auth().currentUser == nil {
             let vc = UINavigationController(rootViewController: OnboardingViewController())
             vc.modalPresentationStyle = .fullScreen
             present(vc, animated: false)
         }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = false
+        handleAuthentication()
+        
     }
 
     
--- TwitterClone/ViewModels/AuthenticationViewViewModel.swift
+++ TwitterClone/ViewModels/AuthenticationViewViewModel.swift
@@ -9,25 +9,25 @@ import Foundation
 import Firebase
 import Combine
 
-final class RegisterViewViewModel: ObservableObject{
+final class AuthenticationViewViewModel: ObservableObject{
     
     
     @Published var email: String?
     @Published var password: String?
-    @Published var isRegistrationFormValid: Bool = false
+    @Published var isAuthenticationFormValid: Bool = false
     @Published var user: User?
-    
+    @Published var error: String?
     
     private var subscriptions: Set<AnyCancellable> = []
     
-     func validateRegistrationForm(){
+     func validateAuthenticationForm(){
          guard let email = email,
                 let password = password else{
-             isRegistrationFormValid = false
+             isAuthenticationFormValid = false
              return
          }
          
-         isRegistrationFormValid = isValidEmail(email) && password.count >= 8
+         isAuthenticationFormValid = isValidEmail(email) && password.count >= 8
     }
     
     func isValidEmail(_ email: String) -> Bool {
@@ -44,7 +44,11 @@ final class RegisterViewViewModel: ObservableObject{
             return
         }
         AuthManager.shared.registerUser(with: email, password: password)
-            .sink { _ in
+            .sink { [weak self] completion in
+                
+                if case .failure(let error)  = completion{
+                    self?.error = error.localizedDescription
+                }
                 
             } receiveValue: { [weak self] user in
                 self?.user = user
@@ -53,6 +57,26 @@ final class RegisterViewViewModel: ObservableObject{
 
     }
     
+    
+    func loginUser(){
+        
+        guard let email = email,
+              let password = password else {
+            return
+        }
+        AuthManager.shared.loginUser(with: email, password: password)
+            .sink { [weak self] completion in
+                
+                if case .failure(let error)  = completion{
+                    self?.error = error.localizedDescription
+                }
+                
+            } receiveValue: { [weak self] user in
+                self?.user = user
+            }
+            .store(in: &subscriptions)
+
+    }
 }
 
 
